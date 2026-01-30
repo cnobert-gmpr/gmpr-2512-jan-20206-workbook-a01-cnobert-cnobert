38da99e42c5c78e4b17c64645beea2fc54ba1f1b
cnobert <cnobert@nait.ca>
2026-01-30 11:31:57 -0700

    bumper

--- Assets/GMPR2512/Lesson05Coroutines/DeathZone.cs
+++ Assets/GMPR2512/Lesson05Coroutines/DeathZone.cs
@@ -7,6 +7,7 @@ namespace GMPR2512.Lesson05Coroutines
     public class DeathZone : MonoBehaviour
     {
         [SerializeField] private Transform _spawnPoint;
+
         void OnTriggerEnter2D(Collider2D collider2D)
         {
             if(collider2D.CompareTag("Ball"))
@@ -20,11 +21,14 @@ namespace GMPR2512.Lesson05Coroutines
         // "StartCoroutine must be passed a method that returns an IEnumerator
         private IEnumerator RespawnBall(GameObject ball)
         {
-            
             yield return new WaitForSeconds(2);
+
             Rigidbody2D ballRB = ball.GetComponent<Rigidbody2D>();
-            ballRB.linearVelocity = Vector2.zero;
-            ballRB.angularVelocity = 0;
+            if(ballRB != null)
+            {
+                ballRB.linearVelocity = Vector2.zero;
+                ballRB.angularVelocity = 0;
+            }
             ball.transform.position = _spawnPoint.position;
         }
     }
